089b91cd76d3c1ffacd951ce151a43fbc9fecb3f
ry0y4n <rusk.ryoy4n@gmail.com>
2024-09-09 00:11:41 +0900

    update: front ui

--- apps/web-extension/src/App.tsx
+++ apps/web-extension/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+
+type Status = "uninitialized" | "inProgress" | "completed";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [generationStatus, setGenerationStatus] =
+    useState<Status>("uninitialized");
+
+  const startProcess = () => {
+    // ステータスを生成中に変更
+    setGenerationStatus("inProgress");
+
+    // シミュレーションとして、3秒後に生成後に変更
+    setTimeout(() => {
+      setGenerationStatus("completed");
+    }, 3000);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {(() => {
+        switch (generationStatus) {
+          case "uninitialized":
+            return (
+              <div>
+                <button onClick={startProcess}>Start</button>
+              </div>
+            );
+          case "inProgress":
+            return <div>Generation in progress...</div>;
+          case "completed":
+            return <div>Generation completed!</div>;
+        }
+      })()}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
