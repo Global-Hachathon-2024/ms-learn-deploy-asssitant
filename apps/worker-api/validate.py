69c8098da3d3945eadc244dcaaa6ae5b4362e6a2
Masaya Sonobe <masayasonobe@microsoft.com>
2024-09-15 14:04:03 +0900

    change DB operations to connect to front-api

--- apps/worker-api/validate.py
+++ apps/worker-api/validate.py
@@ -1,3 +1,12 @@
-# TODO: 内部的にAzure CLIを使うか、GitHub Actionsに渡すかして検証したい
-def validate_arm_template(template: str) -> bool:
-    return True
+from typing import Tuple
+
+def validate_bicep(bicep: str) -> Tuple[bool, str]:
+    """
+    Validate the given Bicep file
+    Args:
+        bicep (str): Bicep file content
+    Returns:
+        bool: True if the Bicep file is valid, False otherwise
+        str: Error message if the Bicep file is invalid
+    """
+    return True, "error message"
