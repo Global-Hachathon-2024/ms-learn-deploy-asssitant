fade2bd818a6be16de46279b29efe838787678f0
Masaya Sonobe <masayasonobe@microsoft.com>
2024-09-17 23:43:35 +0900

    fix: load env vars, change stored path

--- apps/worker-api/save_repository.py
+++ apps/worker-api/save_repository.py
@@ -3,8 +3,12 @@ import re
 
 from github import Github, Auth
 
+from dotenv import load_dotenv
+
+load_dotenv()
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-REPOSITORY_URL = 'msl-autogen-templates'
+repo_owner = 'Global-Hachathon-2024'
+repo_name = 'msl-autogen-templates'
 
 def push_to_github(bicep: str, url: str, **kwargs) -> None:
     """
@@ -20,10 +24,9 @@ def push_to_github(bicep: str, url: str, **kwargs) -> None:
     if not os.path.exists(bicep):
         raise FileNotFoundError('The file does not exist')
 
-    # TODO: use the valid token
     auth = Auth.Token(GITHUB_TOKEN)
     g = Github(auth=auth)
-    repo = g.get_user().get_repo(REPOSITORY_URL)
+    repo = g.get_user(repo_owner).get_repo(repo_name)
 
     with open(bicep, 'r') as file:
         content = file.read()
@@ -31,6 +34,7 @@ def push_to_github(bicep: str, url: str, **kwargs) -> None:
     stored_path = make_stored_path(url)
     full_path = f'templates/{stored_path}/main.bicep'
     commit_message = f'generate a bicep automatically for {url}'
+    # TODO: if the file already exists, update the file
     repo.create_file(full_path, commit_message, content)
 
     params = kwargs.get('params')
@@ -64,15 +68,10 @@ def make_stored_path(url: str) -> str:
         if '?tabs=' in last_path:
             repos_path, params = last_path.split('?tabs=')
             replaced_params = params.replace(',', '+')
+            replaced_params = params.replace('%2C', '+')
             return f"{repos_path}+{replaced_params}"
         else:
             repos_path = last_path
             return f"{repos_path}"
     else:
         raise ValueError(f"Invalid URL: {url}")
-
-# TODO: delete the following code
-push_to_github(
-    './generated/main.bicep', 
-    'https://learn.microsoft.com/en-us/azure/azure-functions/create-first-function-cli-python?tabs=linux%2Cbash%2Cazure-cli%2Cbrowser'
-)
