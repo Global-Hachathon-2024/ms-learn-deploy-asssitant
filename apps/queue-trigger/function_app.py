7e72af5b3aab048f88e4c3d7a6a7af592ff4c8f4
Masaya Sonobe <masayasonobe@microsoft.com>
2024-09-19 01:24:28 +0900

    fix: get raw url from queue message

--- apps/queue-trigger/function_app.py
+++ apps/queue-trigger/function_app.py
@@ -1,32 +1,22 @@
 import logging
 import os
-import json
-
 import azure.functions as func
 from dotenv import load_dotenv
 import requests
 
 load_dotenv()
-AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 WORKER_API_URL = os.getenv("WORKER_API_URL")
 app = func.FunctionApp()
 
+@app.function_name(name="QueueTrigger")
 @app.queue_trigger(arg_name="msg", 
                 queue_name="job-queue",
-                connection=AZURE_STORAGE_CONNECTION_STRING) 
+                connection="Storage") 
 def queue_trigger(msg: func.QueueMessage):
-    logging.info('Python Queue trigger processed a message: %s',
-                msg.get_body().decode('utf-8'))
+    url = msg.get_body().decode('utf-8')
+    print(f"Python Queue trigger processed a message: {url}")
     try:
-        target_url = get_target_url(msg)
-        res = requests.post(f"{WORKER_API_URL}/templates", params={"url": target_url})
-        logging.info(res.text)
+        res = requests.post(f"{WORKER_API_URL}/templates", params={"url": url})
+        logging.info('Worker API response: %s', res.text)
     except Exception as e:
         logging.error(e)
-
-def get_target_url(msg: func.QueueMessage) -> str:
-    data = msg.get_json()
-    if data["target_url"]:
-        return data["target_url"]
-    else:
-        raise ValueError("No target_url provided")
