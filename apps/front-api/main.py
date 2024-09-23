f6b9d154ed18428390298b0a8fcdda3eeca426cb
ry0y4n <rusk.ryoy4n@gmail.com>
2024-09-23 11:18:05 +0900

    add: generate endpoint

--- apps/front-api/main.py
+++ apps/front-api/main.py
@@ -4,6 +4,10 @@ import datetime
 from azure.storage.queue import QueueServiceClient
 from database import DatabaseClient, Result
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class GenerateRequest(BaseModel):
+    url: str
 
 app = FastAPI()
 
@@ -26,8 +30,6 @@ async def poll_status(url: str):
 
     status = db_client.get_result(result.category, result.url_hash)
     if status == None: # not in database yet
-        db_client.insert(result)
-        queue_client.send_message(url)
         return {"status": "uninitialized", "url": ""}
     else: 
         if status["inProgress"]: # inprogress
@@ -37,6 +39,18 @@ async def poll_status(url: str):
                 return {"status": "completed", "url": status["storedUrl"]}
             else:
                 return {"status": "invalid", "url": status["storedUrl"]}
+
+@app.post("/generate")
+async def generate(url_request: GenerateRequest):
+    url = convert_to_en_us_url(url_request.url)
+    result = Result(url, datetime.datetime.now())
+    db_client = DatabaseClient(connection_string)
+
+    status = db_client.get_result(result.category, result.url_hash)
+    if status == None:
+        db_client.insert(result)
+        queue_client.send_message(url)
+    return {"status": "inProgress", "url": ""}
 
 def convert_to_en_us_url(url):
     converted_url = re.sub(r'(https://learn\.microsoft\.com/)([^/]+/)', r'\1en-us/', url)
