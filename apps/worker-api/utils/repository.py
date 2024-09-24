3bb5fc0d3a7e413aaae9321bb1e6f68cbc2e1a47
Masaya Sonobe <masayasonobe@microsoft.com>
2024-09-25 00:59:59 +0900

    fix repository

--- apps/worker-api/utils/repository.py
+++ apps/worker-api/utils/repository.py
@@ -3,13 +3,15 @@ from pathlib import Path
 
 from database import make_stored_dirpath
 
-from github import Github, Auth, Repository
+from github import Github, Auth, Repository, InputGitAuthor
 from dotenv import load_dotenv
 
 load_dotenv(Path(__file__).parent / "../.env")
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 repo_owner = 'Global-Hachathon-2024'
 repo_name = 'msl-autogen-templates'
+github_bot_name = 'github-actions[bot]'
+github_bot_email = 'users.noreply.github.com'
 
 def push_to_github(url: str, bicep: str, params: str) -> None:
     """
@@ -31,12 +33,7 @@ def push_to_github(url: str, bicep: str, params: str) -> None:
     auth = Auth.Token(GITHUB_TOKEN)
     g = Github(auth=auth)
     repo = g.get_user(repo_owner).get_repo(repo_name)
-
-    try:
-        push(repo, bicep, url)
-    except:
-        print('Failed to push the bicep file')
-        raise ValueError('Failed to push the bicep file')
+    push(repo, bicep, url)
 
     if params != None:
         try:
@@ -60,11 +57,30 @@ def push(repo: Repository, path: str, url: str) -> None:
     full_path = f'{stored_dir}/{filename}'
     commit_message = f'generate a {filename} automatically for {url}'
 
+    contents = None
     try:
+        print(f"try to get the file {full_path} from the repository")
         contents = repo.get_contents(full_path)
-        print(f"the file {full_path} already exists and will be updated")
-        repo.update_file(full_path, commit_message, content, contents.sha)
     except:
-        contents = None
         print(f"the file {full_path} does not exist and will be created")
-        repo.create_file(full_path, commit_message, content)
+        author=InputGitAuthor(github_bot_email, github_bot_name)
+        repo.create_file(
+            path=full_path, 
+            message=commit_message, 
+            content=content,
+            branch='main',
+            committer=author,
+            author=author
+        )
+    else:
+        print(f"the file {full_path} already exists and will be updated")
+        author=InputGitAuthor(github_bot_email, github_bot_name)
+        repo.update_file(
+            path=full_path, 
+            message=commit_message, 
+            content=content,
+            sha=contents.sha,
+            branch='main',
+            committer=author,
+            author=author
+        )
